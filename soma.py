5df0184840176741bcd676827e9335564ecc7573
andre75021 <andrelluiz.rodrigues@gmail.com>
2021-05-05 19:48:34 -0300

    update 05/21

--- soma.py
+++ soma.py
@@ -1,7 +1,7 @@
+"""Teste."""
 lado = int(input("Digite o valor correspondente ao lado de um quadrado: "))
 
 perimetro = 4 * lado
 area = lado * lado
-
-print("perimetro:",perimetro,"-","área:",area)
-              
+print("perimetro:", perimetro, "-", "área:", area)
+print("Hello wolrd")
--- testes_gerais.py
+++ testes_gerais.py
@@ -1,9 +1,25 @@
-from functions import cria_lista
-try:
-    tamanho = int(input('Qual o tamanho da lista? '))
-except:
-    print('Entrada inválida!')
+def minimo(lista):
+    var_minimo = None
+    print(f"Antes.: {var_minimo}")
+    for intervar in lista:
+        if (var_minimo == None) or intervar < var_minimo:
+            var_minimo = intervar
+        print(f"Laço.: {intervar} {var_minimo}")
+    print(f"Mínimo.: {var_minimo}")
 
-lista = cria_lista(tamanho)
 
-print(lista)
+
+def maximo(lista):
+    var_maximo = None
+    print(f"Antes: {var_maximo}")
+    for intervar in lista:
+        if (var_maximo == None) or intervar > var_maximo:
+            var_maximo = intervar
+            print(f"Laço: {intervar} {var_maximo}")
+    print(f"Máximo: {var_maximo}")
+
+a = [3,41,12,9,74,15]
+
+maximo(a)
+print()
+minimo(a)
